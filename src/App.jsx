3c411562d0c867652e2f6f121197a48e6feab10a
guillaume45 <couturier.g@gmail.com>
2021-02-04 12:54:08 +0100

    update branch main with the structure website

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import AdminList from './components/admin/AdminList';
+
+
+function App() {
+  return (
+    <div className="App">
+      <AdminList />
+      <h1>hello</h1>
+    </div>
+  );
+}
+
+export default App;
